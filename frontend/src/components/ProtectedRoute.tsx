ba0fcfef6583df9c6d9301b308671609fa88553a
MikePerth <mike@financewa.com.au>
2026-09-04 13:11:05 +0800

    Modified ProtectedRoute.tsx

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute(
     {
         children
     }: {
-        children: JSX.Element
+        children: React.ReactNode
     }
 ) {
 
